0fd4cc885dd321767da55da0aeb4d20eb5f384ed
Yugong Ge <geyugong@gmail.com>
2026-07-05 15:29:16 +0800

    chore(database): add reset script

---
+++ reset-database.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+if exist "wine.db" (
+    del /f /q "wine.db"
+    echo Local database wine.db has been deleted.
+) else (
+    echo Local database wine.db does not exist.
+)
+echo It will be recreated with seed data next time you run the system.
+pause
